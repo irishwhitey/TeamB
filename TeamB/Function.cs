4c226fedc2c124289f49c27180727371c42fcd71
Martin White <martin.white@codecomputerlove.com>
2018-12-14 12:32:24 +0000

    Corrected namespace for entry point

--- TeamB/Function.cs
+++ TeamB/Function.cs
@@ -10,7 +10,7 @@ using TeamB;
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
-namespace TeamA
+namespace TeamB
 {
     public class Function
     {
