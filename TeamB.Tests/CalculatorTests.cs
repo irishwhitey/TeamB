072ba66e4ceb1fbfc58a00b7becc0207b5cbbaeb
Martin White <martin.white@codecomputerlove.com>
2019-01-14 09:41:31 +0000

    Reset to single passing

--- TeamB.Tests/CalculatorTests.cs
+++ TeamB.Tests/CalculatorTests.cs
@@ -6,23 +6,10 @@ namespace TeamB.Tests
     public class CalculatorTests
     {
         [TestCase("3 + 4",7)]
-        [TestCase("-3 + 4", 1)]
-        [TestCase("8 / 4", 2)]
-        [TestCase("2 * 4", 8)]
-        [TestCase("10 - 3", 7)]
-        [TestCase("3 + 4 + 2",9)]
-        [TestCase("10 - 4 - 3",3)]
-        [TestCase("24 / 6 / 2", 2)]
-        //[TestCase("(-3) * (-2)",6)]	
         public void ItShouldReturnSomething(string problem, decimal expected)
         {
             Assert.That(new Calculator().GetResult(problem), Is.EqualTo(expected));
         }
-
-        [Test]
-        public void ItShouldNotBlowUp()
-        {
-            new Calculator().GetResult("fred + 1");
-        }
+      
     }
 }
