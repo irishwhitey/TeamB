7e1c0516587e87ad5990274951e6a4f41ee25dcc
Andrew Pattison <andrew.pattison@codecomputerlove.com>
2019-01-14 15:37:56 +0000

    tests: added another test, just because

--- TeamB.Tests/CalculatorTests.cs
+++ TeamB.Tests/CalculatorTests.cs
@@ -6,6 +6,7 @@ namespace TeamB.Tests
     public class CalculatorTests
     {
         [TestCase("10 * 4 * 2", 80)]
+        [TestCase("80 * 1", 80)]
         public void ItShouldReturnSomething(string problem, decimal expected)
         {
             Assert.That(new Calculator().GetResult(problem), Is.EqualTo(expected));
