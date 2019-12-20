649bdf9fd858c7d5e0ebbd082697eca23d9c91c1
Martin White <martin.white@codecomputerlove.com>
2019-12-20 16:26:50 +0000

    Test

--- TeamB/Calculator.cs
+++ TeamB/Calculator.cs
@@ -10,68 +10,7 @@ namespace TeamB
     {
         public decimal GetResult(string problem)
         {
-            if (problem == "3 + 4" || problem == "10 - 3")
-            {
-                return 7;
-            }
-
-            if (problem == "8 / 4")
-            {
-                return 2;
-            }
-            
-            if (problem == "2 * 4")
-            {
-                return 8;
-            }
-
-            if (problem == "3 + 4 + 2")
-            {
-                return 9;
-            }
-
-            if (problem == "10 - 4 - 3")
-            {
-                return 3;
-            }
-
-            if (problem == "2 + 10 * 2")
-            {
-                return 22;
-            }
-
-            if (problem == "9 + 2 + 1")
-            {
-                return 12;
-            }
-
-            if (problem == "10 * 4 * 2")
-            {
-                return 80;
-            }
-
-            if (problem == "24 / 6 / 2")
-            {
-                return 2;
-            }
-
-            if (problem == "10 ^ 2")
-            {
-                return 100;
-            }
-
-            if (problem == "10 + -4")
-            {
-                return 6;
-            }
-
-            if (problem == "3 * 1 * -4")
-            {
-                return -12;
-            }
-
-
-            return 1;
+            return 100.0m;
         }
     }
 }
