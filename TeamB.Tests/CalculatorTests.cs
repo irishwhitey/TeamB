33805c525240b5c7278f0f7a8563680706f61a3a
CODE\matthewhughes <matthew.hughes@codecomputerlove.com>
2019-12-23 11:32:28 +0000

    Test coverage around existing functionality.

--- TeamB.Tests/CalculatorTests.cs
+++ TeamB.Tests/CalculatorTests.cs
@@ -5,6 +5,12 @@ namespace TeamB.Tests
     [TestFixture]
     public class CalculatorTests
     {
-        
+        [Test]
+        public void ItReturnsASingleNumber()
+        {
+            var calc = new Calculator();
+            var result = calc.GetResult("98");
+            Assert.That(result, Is.EqualTo(98));
+        }
     }
 }
