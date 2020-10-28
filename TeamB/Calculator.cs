450c7cd16eff25d1cb4a76a31322f513603f00bc
Martin White <martin.white@codecomputerlove.com>
2020-10-28 21:52:10 +0000

    Reseting codebase

--- TeamB/Calculator.cs
+++ TeamB/Calculator.cs
@@ -7,30 +7,7 @@ namespace TeamB
     {
         public decimal GetResult(string problem)
         {
-            try
-            {
-                if (problem == string.Empty)
-                    return 0;
-
-                if (problem == "5 - 2 - 1")
-                    return 2;
-
-                if (problem == "12 - 1")
-                    return 11;
-
-                if (problem.Contains("*"))
-                {
-                    var splitMultiplication = problem.Split('*');
-                    return splitMultiplication.Aggregate(1, (a, b) => a * int.Parse(b));
-                }
-
-                var splitProblem = problem.Split('+');
-                return splitProblem.Sum(int.Parse);
-            }
-            catch (Exception e)
-            {
-                return 0;
-            }
+            return -2;
         }
     }
 }
