8d79a324052e9e6ef5eabb4cfbdf7f3234425e63
DevOne <DevOne@sycentric.co.uk>
2020-10-29 15:57:25 +0000

    Second commit

--- TeamB.Tests/CalculatorTests.cs
+++ TeamB.Tests/CalculatorTests.cs
@@ -13,10 +13,21 @@ namespace TeamB.Tests
             _calc = new Calculator();
         }
 
+
         [TestCase("1+2", 3 )]
         [TestCase("1+2+3", 6)]
         [TestCase("1 + 2 + 3", 6)]
         public void SimpleAdditionWorks(string expression, decimal answer)
+        {
+            var result = _calc.GetResult(expression);
+            Assert.AreEqual(answer, result);
+        }
+
+
+        [TestCase("1-2", -1)]
+        [TestCase("1-2-3", -4)]
+        [TestCase("6 - 2 - 1", 3)]
+        public void SimpleSubstractionWorks(string expression, decimal answer)
         {
             var result = _calc.GetResult(expression);
             Assert.AreEqual(answer, result);
--- TeamB/Calculator.cs
+++ TeamB/Calculator.cs
@@ -7,11 +7,55 @@ namespace TeamB
     {
         public decimal GetResult(string problem)
         {
+
+
+
             decimal result = 0;
             var numbers = problem.Split("+");
-            foreach (var num in numbers)
+
+            if (numbers.Length > 1)
             {
-                result += Convert.ToDecimal(num);
+                foreach (var num in numbers)
+                {
+                    try
+                    {
+                        result += Convert.ToDecimal(num);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+
+                return result;
+            }
+
+            numbers = problem.Split("-");
+            if (numbers.Length > 1)
+            {
+                bool firstNumber = true;
+                foreach (var num in numbers)
+                {
+                    try
+                    {
+                        if (firstNumber)
+                        {
+                            result = Convert.ToDecimal(num);
+                            firstNumber = false;
+                        }
+                        else
+                        {
+                            result -= Convert.ToDecimal(num);
+                        }
+                        
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+
+                return result;
             }
 
             return result;
