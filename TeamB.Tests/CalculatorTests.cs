767ddbb4494d55b14f5dab3e4299c908cdaf4510
Martin White <martin.white@codecomputerlove.com>
2020-10-29 10:04:47 +0000

    Reset solution

--- TeamB.Tests/CalculatorTests.cs
+++ TeamB.Tests/CalculatorTests.cs
@@ -5,11 +5,6 @@ namespace TeamB.Tests
     [TestFixture]
     public class CalculatorTests
     {
-        [Test]
-        public void BasicMultiplyTest()
-        {
-            decimal result = new Calculator().GetResult("3 * 2");
-            Assert.That(6, Is.EqualTo(result));
-        }
+
     }
 }
--- TeamB/Calculator.cs
+++ TeamB/Calculator.cs
@@ -7,19 +7,7 @@ namespace TeamB
     {
         public decimal GetResult(string problem)
         {
-            try
-            {
-                if (problem.Contains("*"))
-                {
-                    var split = problem.Split("*");
-                    return decimal.Parse(split[0]) * decimal.Parse(split[1]);
-                }
-            }
-            catch (Exception e)
-            {
-
-            }
-            return -6;
+            return -1;
         }
     }
 }
