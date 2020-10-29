7a04861868e5ff9a7bc763ab636e4f77fcdf87d9
Martin White <martin.white@codecomputerlove.com>
2020-10-29 09:15:27 +0000

    testing commit

--- TeamB.Tests/CalculatorTests.cs
+++ TeamB.Tests/CalculatorTests.cs
@@ -5,29 +5,11 @@ namespace TeamB.Tests
     [TestFixture]
     public class CalculatorTests
     {
-        [TestCase("98", 98)]
-        [TestCase("0", 0)]
-        [TestCase("", 0)]
-        [TestCase("5 + 4", 9)]
-        [TestCase("93 + 1", 94)]
-        [TestCase("2 + 2 + 3", 7)]
-        [TestCase("93 + -1", 92)]
-        [TestCase("5 - 2 - 1", 2)]
-        [TestCase("12 - 1", 11)]
-        [TestCase("3 * 3", 9)]
-        [TestCase("5 * 4", 20)]
-        public void ItCalculatesValidProblem(string problem, int answer)
-        {
-            var calc = new Calculator();
-            var result = calc.GetResult(problem);
-            Assert.That(result, Is.EqualTo(answer));
-        }
-
         [Test]
-        public void ItDoesNotBlowUp()
+        public void BasicMultiplyTest()
         {
-            var calc = new Calculator();
-            Assert.DoesNotThrow(() => calc.GetResult("(1+3) * (7+9)"));
+            decimal result = new Calculator().GetResult("3 * 2");
+            Assert.That(6, Is.EqualTo(result));
         }
     }
 }
--- TeamB/Calculator.cs
+++ TeamB/Calculator.cs
@@ -7,6 +7,11 @@ namespace TeamB
     {
         public decimal GetResult(string problem)
         {
+            if (problem.Contains("*"))
+            {
+                var split = problem.Split("*");
+                return decimal.Parse(split[0]) * decimal.Parse(split[1]);
+            }
             return -2;
         }
     }
